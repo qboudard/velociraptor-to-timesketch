8e9b20d58f55feeb60ec833d1b4cacd62fd6837e
Whitney Champion <whitney.ellis.champion@gmail.com>
2021-10-04 21:58:46 -0400

    adding velociraptor labels for use in sketch name

--- bash/watch-to-timesketch.sh
+++ bash/watch-to-timesketch.sh
@@ -5,8 +5,10 @@ process_files () {
     ZIP=$1
     
     # Get system name
-    SYSTEM=$(echo $ZIP|cut -d"." -f 1)
     SYSTEM=${ZIP%.*}
+
+    # Velociraptor artifact inserts `_$label` to the zip so we can use the label as the sketch name, remove this and references to $LABEL if you do not wish to use it
+    LABEL=$(echo $SYSTEM|cut -d"_" -f 4)
 
     # Unzip
     echo A | unzip $PARENT_DATA_DIR/$ZIP -d $PARENT_DATA_DIR/$SYSTEM
@@ -21,13 +23,13 @@ process_files () {
     docker exec -i timesketch_timesketch-worker_1 /bin/bash -c "log2timeline.py --status_view window --storage_file /usr/share/timesketch/upload/plaso/$SYSTEM.plaso /usr/share/timesketch/upload/$SYSTEM"
     
     # Wait for file to become available
-    sleep 10 
+    sleep 20
 
     # Run timesketch_importer to send Plaso data to Timesketch
-    docker exec -i timesketch_timesketch-worker_1 /bin/bash -c "timesketch_importer -u $username -p "$password" --host http://timesketch-web:5000 --timeline_name $SYSTEM --sketch_name $SYSTEM /usr/share/timesketch/upload/plaso/$SYSTEM.plaso"
+    docker exec -i timesketch_timesketch-worker_1 /bin/bash -c "timesketch_importer -u $username -p "$password" --host http://timesketch-web:5000 --timeline_name $SYSTEM --sketch_name $LABEL /usr/share/timesketch/upload/plaso/$SYSTEM.plaso"
 
     # Copy Plaso files to dir being watched to upload to S3
-    cp -ar /usr/share/timesketch/upload/plaso/$SYSTEM.plaso /usr/share/timesketch/upload/plaso_complete
+    cp -ar /opt/timesketch/upload/plaso/$SYSTEM.plaso /opt/timesketch/upload/plaso_complete/
 }
 
 inotifywait -m -r -e move "$PARENT_DATA_DIR" --format "%f" | while read ZIP
